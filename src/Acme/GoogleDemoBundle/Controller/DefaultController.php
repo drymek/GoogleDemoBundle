007a6b8f653b4bfe5f6d43f5072ee8749475fb1e
Marcin Dryka <marcin@dryka.pl>
2011-10-02 22:52:50 +0200

    Second map prepared

--- src/Acme/GoogleDemoBundle/Controller/DefaultController.php
+++ src/Acme/GoogleDemoBundle/Controller/DefaultController.php
@@ -16,6 +16,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $map = new Map();
+        $map->setTemplating($this->get('templating'));
         $map->setId("demo-map");
 
         $marker = new Marker();
@@ -23,18 +24,33 @@ class DefaultController extends Controller
         $marker->setLongitude(18.67138);
         $marker->setMeta(array(
             'label' => 'x',
-            'infoWindow' => "<div>TEST</div>"
+            'infowindow' => "<div>TEST</div>"
         ));
         $map->addMarker($marker);
 
         $marker = new Marker();
         $marker->setLatitude(50.267848);
         $marker->setLongitude(19.036560);
+        $marker->setMeta(array(
+            'infowindow' => '<div><b>TEST</b></div>',
+        ));
         $map->addMarker($marker);
-        $map->setZoom(10);
 
-        $this->container->get('google.maps')->addMap($map);
+        $this->get('google.maps')->addMap($map);
 
+        /*
+        $map2 = new Map();
+        $map2->setTemplating($this->get('templating'));
+        $map2->setId("demo-map-click");
+
+        $marker = new Marker();
+        $marker->setLatitude(50.294492);
+        $marker->setLongitude(18.67138);
+        $map2->setCenter($marker);
+        $this->setZoom(10);
+
+        $this->get('google.maps')->addMap($map2);
+        */
         return $this->render('AcmeGoogleDemoBundle:Default:index.html.twig', array(
         ));
     }
